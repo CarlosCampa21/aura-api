f14747bfc862bd814d99bcd045c802e744507fb9
MzMendoza <jmendoza_21@alu.uabcs.mx>
2025-11-06 11:06:07 -0700

    feat: wrappers para RAG

---
+++ rag/__init__.py
@@ -0,0 +1,8 @@
+"""Wrappers finos para operar el RAG desde CLI.
+
+Ejemplos:
+  python -m rag.ingest --doc-id <id>
+  python -m rag.retrieve --q "pregunta" --k 5
+  python -m rag.answer --q "pregunta"
+"""
+
